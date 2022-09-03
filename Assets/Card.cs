94a8878336d4653fac044aa48c139029639119a4
g-erm <tanyuting.gt@gmail.com>
2022-09-03 23:07:12 +0800

    Add card effect prototype and card asset prototype and create canvas prefab

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -6,9 +6,19 @@ using UnityEngine.UI;
 public class Card : MonoBehaviour
 {
     public Image currentCard;
+    public CardEffect cardEffect;
+
+    private void Start()
+    {
+        currentCard = gameObject.GetComponent<Image>();
+        gameObject.GetComponent<Button>().onClick.AddListener(UseCard);
+    }
 
     public void UseCard()
     {
         currentCard.gameObject.SetActive(false);
+        if (cardEffect) {
+            cardEffect.TriggerEffect();
+        }
     }
 }
