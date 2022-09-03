a9ddb039bfbf38e7a9f477b8c93c61c4c0abf3aa
g-erm <tanyuting.gt@gmail.com>
2022-09-03 23:07:12 +0800

    Add card effect prototype and card asset prototype and create canvas prefab

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,19 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<Card> deck = new List<Card>();
     public Transform[] cardSlots;
+    public Transform cardGroup;
 
     public void ShuffleCard()
     {
-        foreach (Card c in deck)
-        {
-            c.gameObject.SetActive(false);
-        }
-
         System.Random r = new System.Random();
 
         // Fisher-Yates Shuffle
@@ -24,6 +21,14 @@ public class GameManager : MonoBehaviour
             deck[n] = deck[k];
             deck[k] = temp;
         }
+    }
+
+    public void DrawCard()
+    {
+        foreach (Card c in deck)
+        {
+            c.gameObject.SetActive(false);
+        }
 
         for (int i = 0; i < cardSlots.Length; i++)
         {
@@ -32,5 +37,26 @@ public class GameManager : MonoBehaviour
             randCard.gameObject.SetActive(true);
             randCard.transform.position = cardSlots[i].position;
         }
+    }
+
+    public void AddCard(Card newCard)
+    {
+        deck.Add(newCard);
+        ShuffleCard();
+    }
+
+    public void CreateNewColourCard()
+    {
+        GameObject cardObject = DefaultControls.CreateButton(
+            new DefaultControls.Resources());
+        cardObject.transform.SetParent(cardGroup);
+        cardObject.AddComponent<Card>();
+
+        cardObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+        cardObject.GetComponent<RectTransform>().sizeDelta = new Vector2(70, 100);
+        cardObject.GetComponent<Image>().color = new Color(255, 0, 145);
+
+        cardObject.SetActive(false);
+        AddCard(cardObject.GetComponent<Card>());
     }
 }
