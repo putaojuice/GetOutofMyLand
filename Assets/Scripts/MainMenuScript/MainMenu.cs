c806a16b640ff6469b0f3ec0f355491155d33642
Yuki <yuki.neo34@gmail.com>
2022-09-20 23:34:16 +0800

    implemented working exit popup and volume mixer

--- Assets/Scripts/MainMenuScript/MainMenu.cs
+++ Assets/Scripts/MainMenuScript/MainMenu.cs
@@ -10,9 +10,10 @@ public class MainMenu : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
- /*   public void ExitGame ()
+    public void ExitGame()
     {
-        
-    }*/
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
 
 }
