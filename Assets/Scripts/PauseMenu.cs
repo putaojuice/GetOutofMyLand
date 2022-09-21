22e86b9613ea90df3bb243ad815a440c4ff574dc
g-erm <tanyuting.gt@gmail.com>
2022-09-21 21:48:39 +0800

    Add UI elements

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -23,7 +23,7 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    private void Resume()
+    public void Resume()
     {
         pauseMenuUI.SetActive(false);
         if (!gameObject.GetComponent<PauseWaveButton>().WaveIsPaused)
