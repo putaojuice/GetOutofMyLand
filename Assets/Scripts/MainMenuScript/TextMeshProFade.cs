d817e5e09cceaf9f1f4f8651a02f4c3f39072cda
Yuki <yuki.neo34@gmail.com>
2022-10-11 20:27:50 +0800

    Made the auto loop fade in out animation working

--- Assets/Scripts/MainMenuScript/TextMeshProFade.cs
+++ Assets/Scripts/MainMenuScript/TextMeshProFade.cs
@@ -13,15 +13,37 @@ public class TextMeshProFade : MonoBehaviour
 
     private IEnumerator FadeOut()
     {
-        float duration = 2f; //Fade out over 2 seconds.
-        float currentTime = 0f;
-        while (currentTime < duration)
-        {
-            float alpha = Mathf.Lerp(0f, 1f, currentTime / duration);
-            textDisplay.color = new Color(textDisplay.color.r, textDisplay.color.g, textDisplay.color.b, alpha);
-            currentTime += Time.deltaTime;
-            yield return null;
+        bool isFadeOut = true;
+        while (true) { 
+            float duration = 2f; //Fade out over 2 seconds.
+            float currentTime = 0f;
+            if (isFadeOut)
+            {
+            while (currentTime < duration)
+            {
+                float alpha = Mathf.Lerp(0f, 1f, currentTime / duration);
+                textDisplay.color = new Color(textDisplay.color.r, textDisplay.color.g, textDisplay.color.b, alpha);
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+            isFadeOut = false;
+            Debug.Log("fade out");
+                continue;
+        } else {
+            while (currentTime < duration)
+            {
+                float alpha = Mathf.Lerp(1f, 0f, currentTime / duration);
+                textDisplay.color = new Color(textDisplay.color.r, textDisplay.color.g, textDisplay.color.b, alpha);
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+
+            isFadeOut = true;
+            Debug.Log("fade in");
+                continue;
         }
-        yield break;
+        }
     }
+
+
 }
