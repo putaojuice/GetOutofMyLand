18f456da05870427363a6d8df512003a212134f4
Hzxin <61725744+Hzxin@users.noreply.github.com>
2022-10-13 21:02:27 +0800

    Add Count down

--- Assets/Scripts/Music/MusicControlScript.cs
+++ Assets/Scripts/Music/MusicControlScript.cs
@@ -8,6 +8,7 @@ public class MusicControlScript : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip ambient;
     [SerializeField] private AudioClip funky;
+    [SerializeField] private AudioSource CountDownSource;
 
 
     void Start() 
@@ -21,10 +22,35 @@ public class MusicControlScript : MonoBehaviour
         audioSource.Play();
     }
 
+    public void PlayCountDown()
+    {
+        CountDownSource.Play();
+    }
+
     public void PlayFunky()
     {
         audioSource.clip = funky;
         audioSource.Play();
+    }
+
+    public void fadeAudio()
+    {
+        StartCoroutine(StartFade(audioSource, 3f, 0));
+    }
+
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float originalVolume = audioSource.volume;
+        float start = audioSource.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        audioSource.volume = originalVolume;
+        yield break;
     }
 
 }
--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -16,11 +16,10 @@ public class GameManager : MonoBehaviour
     [SerializeField]private DeckController DeckController;
     [SerializeField]private EnemyManager EnemyManager;
     [SerializeField]private Button spawnButton;
-    [SerializeField]
-    private Button wavePauseButton;
+    [SerializeField]private Button wavePauseButton;
     [SerializeField]private GameObject gameOverUI;
     [SerializeField]private MusicControlScript MusicController;
-
+    [SerializeField]private GameObject CountDownText;
     public Transform cardGroup;
     public int currentHandSize;
 
@@ -31,25 +30,37 @@ public class GameManager : MonoBehaviour
     }
 
     void Start() {
-        DeckController = GetComponent<DeckController>();
         DeckController.ShuffleCard();
         DeckController.DrawCard();
         spawnButton.onClick.AddListener(TaskOnClick);
         wavePauseButton.interactable = false;
-
         waveIndex = 0;
+    }
+
+    IEnumerator CountDown (int countDown) {
+     int counter = countDown;
+     CountDownText.SetActive(true);
+     MusicController.fadeAudio();
+     MusicController.PlayCountDown();
+     while (counter > 0) {
+         CountDownText.GetComponent<TMP_Text>().text = counter.ToString();
+         yield return new WaitForSeconds (1);
+         counter--;
+     }
+     CountDownText.SetActive(false);
+     SpawnEnemy();
+     spawnButton.interactable = false;
+
+     DeckController.disableHand();
+     wavePauseButton.interactable = true;
+
+     //Music Stuff
+     MusicController.PlayFunky();
     }
 
     void TaskOnClick()
     {   
-
-        SpawnEnemy();
-        spawnButton.interactable = false;
-        DeckController.disableHand();
-        wavePauseButton.interactable = true;
-
-        //Music Stuff
-        MusicController.PlayFunky();
+        StartCoroutine(CountDown(3));
 
     }
 
