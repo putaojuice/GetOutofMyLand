c1f83e8536a65f8667035b4b26c642b7c7da8be1
LimZeWeiDennis <dennislimzw@gmail.com>
2022-10-22 12:36:51 +0800

    HP and charge bar faces camera

--- Assets/Scripts/Enemy/EaterChargeBar.cs
+++ Assets/Scripts/Enemy/EaterChargeBar.cs
@@ -8,17 +8,21 @@ public class EaterChargeBar : MonoBehaviour
     [SerializeField] private float chargePoints;
     [SerializeField] private Eater eater;
     [SerializeField] private RectTransform rect;
+    [SerializeField] public Camera mainCamera;
+    [SerializeField] public Canvas mainCanvas;
     // Start is called before the first frame update
     void Start()
     {
         maxCharge = eater.maxChargePoints;
         chargePoints = eater.chargePoints;
         rect = gameObject.GetComponent<RectTransform>();
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        mainCanvas.transform.LookAt(transform.position + mainCamera.GetComponent<Camera>().transform.rotation * Vector3.forward, mainCamera.GetComponent<Camera>().transform.rotation * Vector3.up);
         chargePoints = eater.chargePoints;
         rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, chargePoints/maxCharge * 90);
     }
