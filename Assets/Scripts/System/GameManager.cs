e3f3ff3bda1c5fe52338d24528a8e0f5616119ec
Hzxin <61725744+Hzxin@users.noreply.github.com>
2022-11-01 20:27:00 +0800

    Modify score

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -97,12 +97,12 @@ public class GameManager : MonoBehaviour
 
     public void GameOver() {
         int enemiesScore = EnemyManager.GetEnemiesKilled() * (1 + (waveIndex / 60));
-        int landScore = (GameObject.FindGameObjectsWithTag("GridFloor").Length - 13) * (1 + (waveIndex / 60));
+        int landScore = (GameObject.FindGameObjectsWithTag("GridFloor").Length - 12) * (1 + (waveIndex / 60));
         scoreText.GetComponent<TMP_Text>().text = "Score: " + (enemiesScore + landScore); 
         gemCount.text = "X " + (int) ((enemiesScore + landScore) / 30);
         if ((enemiesScore + landScore) / 30 >= 1)
         {
-            UpgradeManager.instance.data.upgradePoint += (enemiesScore + landScore) / 30;
+            UpgradeManager.instance.data.upgradePoint += (enemiesScore + landScore) / 40;
             UpgradeManager.instance.Save();
         }
         gameOverUI.SetActive(true);
